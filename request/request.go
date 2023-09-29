f7afd8882c6476489cd1c21fc508cc9e6c146b8f
MatthieuLvsr <72619668+MatthieuLvsr@users.noreply.github.com>
2023-09-29 12:40:18 +0200

    quasi fini

--- request/request.go
+++ request/request.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+
 	// "os"
 	"sync"
 	"time"
@@ -38,16 +39,54 @@ func Request() {
 					rbody, _ := io.ReadAll(resp.Body)
 					fmt.Printf("port : %d -> %s\n",port,string(rbody))
 					body := []byte(`{
+						"User": "Matt",
+						"Secret": "84a4b19e19aa4e2a562ae0286b1e188ef4f4f9a98a92b8730d20a1e0f2882523"
+					}`)
+					ReqPost(port, "iNeedAHint",body)
+					body = []byte(`{
 						"User": "Matt"
 					}`)
 					ReqPost(port, "signup",body)
+					body = []byte(`{
+						"User": "Matt",
+						"Secret": "84a4b19e19aa4e2a562ae0286b1e188ef4f4f9a98a92b8730d20a1e0f2882523"
+					}`)
+					ReqPost(port, "iNeedAHint",body)
+					body = []byte(`{
+						"User": "Matt"
+					}`)
 					ReqPost(port, "check",body)
 					body = []byte(`{
 						"User": "Matt",
-						"Secret": "admin"
+						"Secret": "84a4b19e19aa4e2a562ae0286b1e188ef4f4f9a98a92b8730d20a1e0f2882523"
+					}`)
+					ReqPost(port, "iNeedAHint",body)
+					ReqPost(port, "getUserSecret",body)
+					body = []byte(`{
+						"User": "Matt",
+						"Secret": "84a4b19e19aa4e2a562ae0286b1e188ef4f4f9a98a92b8730d20a1e0f2882523"
+					}`)
+					ReqPost(port, "iNeedAHint",body)
+					body = []byte(`{
+						"User": "Matt",
+						"Secret": "84a4b19e19aa4e2a562ae0286b1e188ef4f4f9a98a92b8730d20a1e0f2882523"
 					}`)
 					ReqPost(port, "getUserLevel",body)
+					ReqPost(port, "iNeedAHint",body)
+					body = []byte(`{
+						"User": "Matt",
+						"Secret": "84a4b19e19aa4e2a562ae0286b1e188ef4f4f9a98a92b8730d20a1e0f2882523"
+					}`)
 					ReqPost(port, "getUserPoints",body)
+					ReqPost(port, "iNeedAHint",body)
+					body = []byte(`{
+						"Code": "Q DC3 ) 1 4"
+					}`)
+					ReqPost(port, "translate",body)
+					// Tiny Path [ctf-school-Today is a good day innit ?]
+					// Not a binary  Q DC3 ) 1 4
+					// Copy Trash 5FPprcvF-T75f91DQ2C
+					// Dabatase App : 72 44 90
 					// founder(port)
 					return
 				}
